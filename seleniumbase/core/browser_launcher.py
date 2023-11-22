ff1146ee348a40ffeae3938ee323da41abfc791d
Kamil Ali <kamil.mu.ali@gmail.com>
2023-11-22 04:13:45 -0600

    print clearer

--- seleniumbase/core/browser_launcher.py
+++ seleniumbase/core/browser_launcher.py
@@ -3391,7 +3391,7 @@ def get_local_driver(
                                     uc_path = LOCAL_UC_DRIVER
                                     uc_path = os.path.realpath(uc_path)
                                     print(uc_path)
-                                print(b_path)
+                                print(b_path, "binary path")
                                 driver = undetected.Chrome(
                                     options=chrome_options,
                                     user_data_dir=user_data_dir,
